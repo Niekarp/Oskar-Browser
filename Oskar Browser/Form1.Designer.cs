0b4d0a6d2588fe9ac7ebeacbf8e8b41d1987309f
Niekarp <skrzynka255@gmail.com>
2016-05-02 23:29:51 +0200

    pawe

---
+++ Oskar Browser/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Oskar_Browser
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.PageRefreshButton = new System.Windows.Forms.Button();
+            this.PageLoadingButton = new System.Windows.Forms.Button();
+            this.UrlField = new System.Windows.Forms.TextBox();
+            this.UrlFormAndHomePageButtonGrid = new System.Windows.Forms.TableLayoutPanel();
+            this.HomePageButton = new System.Windows.Forms.Button();
+            this.MenuDownLine = new System.Windows.Forms.Label();
+            this.PreviousPageButton = new System.Windows.Forms.Button();
+            this.NextPageButton = new System.Windows.Forms.Button();
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.MenuUpLine = new System.Windows.Forms.Label();
+            this.AddTabButton = new System.Windows.Forms.Button();
+            this.UrlFormAndHomePageButtonGrid.SuspendLayout();
+            this.MenuPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PageRefreshButton
+            // 
+            this.PageRefreshButton.Font = new System.Drawing.Font("Tahoma", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PageRefreshButton.Location = new System.Drawing.Point(178, 13);
+            this.PageRefreshButton.Name = "PageRefreshButton";
+            this.PageRefreshButton.Size = new System.Drawing.Size(34, 27);
+            this.PageRefreshButton.TabIndex = 5;
+            this.PageRefreshButton.Text = "R";
+            this.PageRefreshButton.UseVisualStyleBackColor = true;
+            this.PageRefreshButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RefreshButton_MouseClick);
+            // 
+            // PageLoadingButton
+            // 
+            this.PageLoadingButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.PageLoadingButton.Location = new System.Drawing.Point(218, 13);
+            this.PageLoadingButton.Name = "PageLoadingButton";
+            this.PageLoadingButton.Size = new System.Drawing.Size(34, 27);
+            this.PageLoadingButton.TabIndex = 6;
+            this.PageLoadingButton.UseVisualStyleBackColor = false;
+            // 
+            // UrlField
+            // 
+            this.UrlField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UrlField.BackColor = System.Drawing.SystemColors.Window;
+            this.UrlField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlField.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.UrlField.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.UrlField.Location = new System.Drawing.Point(3, 7);
+            this.UrlField.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.UrlField.Name = "UrlField";
+            this.UrlField.Size = new System.Drawing.Size(587, 26);
+            this.UrlField.TabIndex = 3;
+            this.UrlField.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UrlField_KeyDown);
+            this.UrlField.Leave += new System.EventHandler(this.UrlField_Leave);
+            this.UrlField.MouseUp += new System.Windows.Forms.MouseEventHandler(this.UrlField_MouseUp);
+            // 
+            // UrlFormAndHomePageButtonGrid
+            // 
+            this.UrlFormAndHomePageButtonGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UrlFormAndHomePageButtonGrid.BackColor = System.Drawing.Color.Transparent;
+            this.UrlFormAndHomePageButtonGrid.ColumnCount = 2;
+            this.UrlFormAndHomePageButtonGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.UrlFormAndHomePageButtonGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.UrlFormAndHomePageButtonGrid.Controls.Add(this.UrlField, 0, 0);
+            this.UrlFormAndHomePageButtonGrid.Controls.Add(this.HomePageButton, 1, 0);
+            this.UrlFormAndHomePageButtonGrid.Location = new System.Drawing.Point(258, 7);
+            this.UrlFormAndHomePageButtonGrid.MaximumSize = new System.Drawing.Size(1200, 100);
+            this.UrlFormAndHomePageButtonGrid.Name = "UrlFormAndHomePageButtonGrid";
+            this.UrlFormAndHomePageButtonGrid.RowCount = 1;
+            this.UrlFormAndHomePageButtonGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.UrlFormAndHomePageButtonGrid.Size = new System.Drawing.Size(633, 40);
+            this.UrlFormAndHomePageButtonGrid.TabIndex = 8;
+            // 
+            // HomePageButton
+            // 
+            this.HomePageButton.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.HomePageButton.BackColor = System.Drawing.Color.Transparent;
+            this.HomePageButton.BackgroundImage = global::Oskar_Browser.Properties.Resources.HomePageButton_test1;
+            this.HomePageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HomePageButton.Location = new System.Drawing.Point(596, 3);
+            this.HomePageButton.Name = "HomePageButton";
+            this.HomePageButton.Size = new System.Drawing.Size(34, 34);
+            this.HomePageButton.TabIndex = 7;
+            this.HomePageButton.UseVisualStyleBackColor = false;
+            this.HomePageButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.HomePageButton_MouseClick);
+            // 
+            // MenuDownLine
+            // 
+            this.MenuDownLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuDownLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MenuDownLine.Location = new System.Drawing.Point(-2, 74);
+            this.MenuDownLine.Name = "MenuDownLine";
+            this.MenuDownLine.Size = new System.Drawing.Size(1100, 1);
+            this.MenuDownLine.TabIndex = 9;
+            // 
+            // PreviousPageButton
+            // 
+            this.PreviousPageButton.BackColor = System.Drawing.Color.Transparent;
+            this.PreviousPageButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.PreviousPageButton.Font = new System.Drawing.Font("Lucida Console", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PreviousPageButton.ForeColor = System.Drawing.Color.Black;
+            this.PreviousPageButton.Image = global::Oskar_Browser.Properties.Resources.PreviousPageButton;
+            this.PreviousPageButton.Location = new System.Drawing.Point(3, 8);
+            this.PreviousPageButton.Name = "PreviousPageButton";
+            this.PreviousPageButton.Size = new System.Drawing.Size(86, 35);
+            this.PreviousPageButton.TabIndex = 1;
+            this.PreviousPageButton.UseVisualStyleBackColor = false;
+            this.PreviousPageButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PreviousPage_MouseDown);
+            // 
+            // NextPageButton
+            // 
+            this.NextPageButton.BackColor = System.Drawing.Color.Transparent;
+            this.NextPageButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.NextPageButton.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextPageButton.ForeColor = System.Drawing.Color.Black;
+            this.NextPageButton.Image = global::Oskar_Browser.Properties.Resources.NextPageButton2;
+            this.NextPageButton.Location = new System.Drawing.Point(86, 8);
+            this.NextPageButton.Name = "NextPageButton";
+            this.NextPageButton.Size = new System.Drawing.Size(86, 35);
+            this.NextPageButton.TabIndex = 2;
+            this.NextPageButton.UseVisualStyleBackColor = false;
+            this.NextPageButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.NextPage_MouseDown);
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.MenuPanel.Controls.Add(this.PreviousPageButton);
+            this.MenuPanel.Controls.Add(this.NextPageButton);
+            this.MenuPanel.Controls.Add(this.PageRefreshButton);
+            this.MenuPanel.Controls.Add(this.PageLoadingButton);
+            this.MenuPanel.Controls.Add(this.UrlFormAndHomePageButtonGrid);
+            this.MenuPanel.Location = new System.Drawing.Point(-1, 22);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(1093, 52);
+            this.MenuPanel.TabIndex = 11;
+            // 
+            // MenuUpLine
+            // 
+            this.MenuUpLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MenuUpLine.BackColor = System.Drawing.Color.Transparent;
+            this.MenuUpLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MenuUpLine.Location = new System.Drawing.Point(-3, 21);
+            this.MenuUpLine.Name = "MenuUpLine";
+            this.MenuUpLine.Size = new System.Drawing.Size(1100, 1);
+            this.MenuUpLine.TabIndex = 12;
+            // 
+            // AddTabButton
+            // 
+            this.AddTabButton.BackColor = System.Drawing.SystemColors.Control;
+            this.AddTabButton.FlatAppearance.BorderSize = 0;
+            this.AddTabButton.ForeColor = System.Drawing.Color.Black;
+            this.AddTabButton.Location = new System.Drawing.Point(-2, -2);
+            this.AddTabButton.Name = "AddTabButton";
+            this.AddTabButton.Size = new System.Drawing.Size(31, 26);
+            this.AddTabButton.TabIndex = 13;
+            this.AddTabButton.Text = "+";
+            this.AddTabButton.UseVisualStyleBackColor = false;
+            this.AddTabButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AddTabButton_MouseClick);
+            // 
+            // MainWindow
+            // 
+            this.AccessibleDescription = "";
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(1092, 749);
+            this.Controls.Add(this.MenuUpLine);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.MenuDownLine);
+            this.Controls.Add(this.AddTabButton);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainWindow";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "~Oskar Browser~";
+            this.UrlFormAndHomePageButtonGrid.ResumeLayout(false);
+            this.UrlFormAndHomePageButtonGrid.PerformLayout();
+            this.MenuPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NextPageButton;
+        private System.Windows.Forms.Button PreviousPageButton;
+        private System.Windows.Forms.Button PageRefreshButton;
+        private System.Windows.Forms.Button PageLoadingButton;
+        private System.Windows.Forms.Button HomePageButton;
+        private System.Windows.Forms.TextBox UrlField;
+        private System.Windows.Forms.TableLayoutPanel UrlFormAndHomePageButtonGrid;
+        private System.Windows.Forms.Label MenuDownLine;
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Label MenuUpLine;
+        private System.Windows.Forms.Button AddTabButton;
+    }
+}
+
---
+++ Oskar Browser/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oskar_Browser
+{
+    public partial class MainWindow : Form
+    {
+        private bool _urlFieldLostFocus = true;
+        #region TabThings
+        private readonly Dictionary<Button, Gecko.GeckoWebBrowser> _tabs = new Dictionary<Button, Gecko.GeckoWebBrowser>();
+        private Button _currentTab = null;
+        private string _defaultNewTabPageAdress = "http://www.google.pl/";
+
+        private void AddNewTab()
+        {
+            var newTabButton = new Button();
+            Controls.Add(newTabButton);
+            newTabButton.BackColor = System.Drawing.SystemColors.Control;
+            newTabButton.FlatAppearance.BorderSize = 0;
+            newTabButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            newTabButton.ForeColor = System.Drawing.Color.Black;
+            newTabButton.Location = new System.Drawing.Point(-2 + (160 * _tabs.Count), -2);
+            newTabButton.Name = "TabButton";
+            newTabButton.Size = new System.Drawing.Size(162, 26);
+            newTabButton.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            newTabButton.UseVisualStyleBackColor = false;
+            newTabButton.MouseDown += new System.Windows.Forms.MouseEventHandler(TabClick);
+
+
+            var newBrowserForm = new Gecko.GeckoWebBrowser();
+            Controls.Add(newBrowserForm);
+            newBrowserForm.AllowDrop = true;
+            newBrowserForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            newBrowserForm.Location = new System.Drawing.Point(-1, 74);
+            newBrowserForm.Name = "BrowserForm";
+            newBrowserForm.Size = new System.Drawing.Size(this.Size.Width - 15, this.Size.Height - 113);
+            newBrowserForm.TabIndex = 4;
+            newBrowserForm.UseHttpActivityObserver = false;
+            newBrowserForm.Navigating += new System.EventHandler<Gecko.Events.GeckoNavigatingEventArgs>(BrowserForm_Navigating);
+            newBrowserForm.DocumentCompleted += new System.EventHandler<Gecko.Events.GeckoDocumentCompletedEventArgs>(BrowserForm_DocumentCompleted);
+            newBrowserForm.CanGoBackChanged += new System.EventHandler(BrowserForm_CanGoBackChanged);
+            newBrowserForm.CanGoForwardChanged += new System.EventHandler(BrowserForm_CanGoForwardChanged);
+            newBrowserForm.CreateWindow += new System.EventHandler<Gecko.GeckoCreateWindowEventArgs>(BrowserForm_CreateWindow);
+            newBrowserForm.DomFocus += new System.EventHandler<Gecko.DomEventArgs>(BrowserForm_DomFocus);
+
+            _tabs.Add(newTabButton, newBrowserForm);
+            newBrowserForm.Navigate(_defaultNewTabPageAdress);
+            SwapTab(newTabButton);
+
+            AddTabButton.Location = new System.Drawing.Point(-2 + (160 * _tabs.Count), -2);
+        }
+        private void AddNewTab(string firstUrl)
+        {
+            var newTabButton = new Button();
+            Controls.Add(newTabButton);
+            newTabButton.BackColor = System.Drawing.SystemColors.Control;
+            newTabButton.FlatAppearance.BorderSize = 0;
+            newTabButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            newTabButton.ForeColor = System.Drawing.Color.Black;
+            newTabButton.Location = new System.Drawing.Point(-2 + (160 * _tabs.Count), -2);
+            newTabButton.Name = "TabButton";
+            newTabButton.Size = new System.Drawing.Size(162, 26);
+            newTabButton.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            newTabButton.UseVisualStyleBackColor = false;
+            newTabButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TabClick);
+
+
+            var newBrowserForm = new Gecko.GeckoWebBrowser();
+            Controls.Add(newBrowserForm);
+            newBrowserForm.AllowDrop = true;
+            newBrowserForm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            newBrowserForm.Location = new System.Drawing.Point(-1, 74);
+            newBrowserForm.Name = "BrowserForm";
+            newBrowserForm.Size = new System.Drawing.Size(this.Size.Width - 15, this.Size.Height - 113);
+            newBrowserForm.TabIndex = 4;
+            newBrowserForm.UseHttpActivityObserver = false;
+            newBrowserForm.Navigating += new System.EventHandler<Gecko.Events.GeckoNavigatingEventArgs>(BrowserForm_Navigating);
+            newBrowserForm.DocumentCompleted += new System.EventHandler<Gecko.Events.GeckoDocumentCompletedEventArgs>(BrowserForm_DocumentCompleted);
+            newBrowserForm.CanGoBackChanged += new System.EventHandler(BrowserForm_CanGoBackChanged);
+            newBrowserForm.CanGoForwardChanged += new System.EventHandler(BrowserForm_CanGoForwardChanged);
+            newBrowserForm.CreateWindow += new System.EventHandler<Gecko.GeckoCreateWindowEventArgs>(BrowserForm_CreateWindow);
+            newBrowserForm.DomFocus += new System.EventHandler<Gecko.DomEventArgs>(BrowserForm_DomFocus);
+
+            _tabs.Add(newTabButton, newBrowserForm);
+            newBrowserForm.Navigate(firstUrl);
+            SwapTab(newTabButton);
+
+            AddTabButton.Location = new System.Drawing.Point(-2 + (160 * _tabs.Count), -2);
+        }
+        private void RemoveTab(Button tab)
+        {
+            Controls.Remove(_tabs[tab]);
+            Controls.Remove(tab);
+            _tabs[tab].Dispose();
+            _tabs.Remove(tab);
+
+            if (tab == _currentTab && _tabs.Count > 0)
+            {
+                _currentTab = null;
+                var tabToSrang = _tabs.Keys.OrderBy(btn => Math.Abs(btn.Location.X - tab.Location.X - 1)).First();
+                SwapTab(tabToSrang);
+            }
+
+            var tabsToMove = _tabs.Keys.Where(btn => btn.Location.X > tab.Location.X);
+            foreach (var btn in tabsToMove)
+                btn.Location += new Size(-160, 0);
+
+            if (_tabs.Count == 0)
+            {
+                _currentTab = null;
+                UrlField.Text = "";
+            }
+
+            AddTabButton.Location = new Point(AddTabButton.Location.X - 160, -2);
+        }
+        private void SwapTab(Button tab)
+        {
+            if (_currentTab != null)
+            {
+                _currentTab.BackColor = System.Drawing.SystemColors.Control;
+                _currentTab.FlatAppearance.BorderSize = 0;
+                _currentTab.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+                tab.Size = new Size(162, 26);
+                _tabs[_currentTab].Hide();
+            }
+
+            _currentTab = tab;
+            _tabs[_currentTab].Show();
+            _tabs[_currentTab].Focus();
+            UrlField.Text = (string)_currentTab.Tag;
+            UpdateDirectionButtons();
+
+            tab.BackColor = System.Drawing.SystemColors.ScrollBar;
+            tab.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            tab.Size = new Size(163, 26);
+            tab.SendToBack();
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PreviousPageButton.BackColor = Color.Silver;
+            PreviousPageButton.ForeColor = Color.Red;
+            NextPageButton.BackColor = Color.Silver;
+            NextPageButton.ForeColor = Color.Red;
+
+            AddNewTab();
+            _tabs[_currentTab].Navigate(_defaultNewTabPageAdress);
+        }
+
+        #region Events
+        private void PreviousPage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_currentTab != null)
+            {
+                if (_tabs[_currentTab].CanGoBack)
+                {
+                    _tabs[_currentTab].GoBack();
+                }
+            }
+        }
+        private void NextPage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_currentTab != null)
+            {
+                if (_tabs[_currentTab].CanGoForward)
+                {
+                    _tabs[_currentTab].GoForward();
+                }
+            }
+        }
+        private void UrlField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && _currentTab != null)
+            {
+                if (!UrlField.Text.Contains('.'))
+                {
+                    _tabs[_currentTab].Navigate("https://www.google.pl/?gws_rd=ssl#q=" + UrlField.Text);
+                }
+                else _tabs[_currentTab].Navigate(UrlField.Text);
+
+                _urlFieldLostFocus = true;
+            }
+        }
+        private void BrowserForm_Navigating(object sender, Gecko.Events.GeckoNavigatingEventArgs e)
+        {
+            _tabs[_currentTab].Cursor = Cursors.WaitCursor;
+            PageLoadingButton.BackColor = Color.SandyBrown;
+        }
+        private void RefreshButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_currentTab != null)
+                _tabs[_currentTab].Navigate(_tabs[_currentTab].Url.AbsoluteUri);
+        }
+        private void BrowserForm_DocumentCompleted(object sender, Gecko.Events.GeckoDocumentCompletedEventArgs e)
+        {
+            var actualTab = _tabs.Where(v => v.Value == (sender as Gecko.GeckoWebBrowser)).First().Key;
+            if (actualTab == _currentTab)
+            {
+                UrlField.Clear();
+                _currentTab.Tag = _tabs[_currentTab].Url.AbsoluteUri;
+                UrlField.Text = (string)_currentTab.Tag;
+            }
+            else
+                actualTab.Tag = _tabs[actualTab].Url.AbsoluteUri;
+
+            PageLoadingButton.BackColor = Color.LightSteelBlue;
+
+            actualTab.Text = (sender as Gecko.GeckoWebBrowser).DomDocument.Title;
+        }
+        private void HomePageButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_currentTab != null)
+                _tabs[_currentTab].Navigate("www.google.pl");
+        }
+        private void BrowserForm_CreateWindow(object sender, Gecko.GeckoCreateWindowEventArgs e)
+        {
+            AddNewTab(e.Uri);
+            e.Cancel = true;
+        }
+        private void BrowserForm_CanGoBackChanged(object sender, EventArgs e)
+        {
+            if (_tabs[_currentTab].CanGoBack)
+            {
+                PreviousPageButton.BackColor = Color.Transparent;
+                PreviousPageButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                PreviousPageButton.BackColor = Color.Silver;
+                PreviousPageButton.ForeColor = Color.Red;
+            }
+        }
+        private void BrowserForm_CanGoForwardChanged(object sender, EventArgs e)
+        {
+            if (_tabs[_currentTab].CanGoForward)
+            {
+                NextPageButton.BackColor = Color.Transparent;
+                NextPageButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                NextPageButton.BackColor = Color.Silver;
+                NextPageButton.ForeColor = Color.Red;
+            }
+        }
+        private void UrlField_Leave(object sender, EventArgs e)
+        {
+            _urlFieldLostFocus = true;
+            if (_currentTab != null) _currentTab.Tag = UrlField.Text;
+        }
+        private void UrlField_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!_urlFieldLostFocus) return;
+            UrlField.SelectAll();
+            _urlFieldLostFocus = false;
+        }
+        private void BrowserForm_DomFocus(object sender, Gecko.DomEventArgs e)
+        {
+            if (_currentTab != null)
+                _tabs[_currentTab].Focus();
+        }
+        private void AddTabButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.AddNewTab();
+        }
+        private void TabClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                RemoveTab(sender as Button);
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                SwapTab(sender as Button);
+            }
+        }
+        #endregion
+        private void UpdateDirectionButtons()
+        {
+            if (_tabs[_currentTab].CanGoBack)
+            {
+                PreviousPageButton.BackColor = Color.Transparent;
+                PreviousPageButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                PreviousPageButton.BackColor = Color.Silver;
+                PreviousPageButton.ForeColor = Color.Red;
+            }
+            if (_tabs[_currentTab].CanGoForward)
+            {
+                NextPageButton.BackColor = Color.Transparent;
+                NextPageButton.ForeColor = Color.Black;
+            }
+            else
+            {
+                NextPageButton.BackColor = Color.Silver;
+                NextPageButton.ForeColor = Color.Red;
+            }
+        }
+    }
+}
